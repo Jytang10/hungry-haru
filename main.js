3f97338794355ec2e654bc8aa080cbff0a86d820
James Tang <jytang10@gmail.com>
2019-07-14 00:39:22 +0000

    Added stats tracking for accuracy, games played, attempts

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ let app;
 
 let firstCardClicked = null;
 let secondCardClicked = null;
-let matches = null;
-let max_matches = 2;
+let matches = 0;
+let max_matches = 9;
+let attempts = 0;
+let accuracy;
+let games_played = 0;
 
 function initializeApp() {
-    // app = new Game($('body'));
-    $(".card").on('click', handleCardClick)
+  // app = new Game($('body'));
+  $(".card").on('click', handleCardClick)
+  displayStats();
 }
 
 function handleCardClick( event ) {
@@ -18,8 +22,8 @@ function handleCardClick( event ) {
     firstCardClicked = $(event.currentTarget);
   } else {
     secondCardClicked = $(event.currentTarget);
+    attempts++;
   }
-
   if ($(firstCardClicked).find('.card-front').css('background-image') === $(secondCardClicked).find('.card-front').css('background-image')) {
     console.log('cards match');
     matches++;
@@ -27,10 +31,14 @@ function handleCardClick( event ) {
     $(secondCardClicked).addClass('hidden');
     firstCardClicked = null;
     secondCardClicked = null;
+    displayStats();
     if(matches === max_matches) {
+    games_played++;
+    console.log(games_played)
     $('#winModal').modal('show');
     }
   } else if(firstCardClicked && secondCardClicked && $(firstCardClicked).find('.card-front').css('background-image') !== $(secondCardClicked).find('.card-front').css('background-image')) {
+    displayStats()
     setTimeout(()=>{
       $(firstCardClicked).find('.card-back').removeClass('hidden');
       $(secondCardClicked).find('.card-back').removeClass('hidden');
@@ -38,4 +46,21 @@ function handleCardClick( event ) {
       secondCardClicked = null;
     }, 1000);
   }
+}
+
+function calculateAccuracy() {
+  if (attempts) {
+    accuracy = ((matches / attempts) * 100)
+  } else {
+    accuracy = 0
+  }
+  return accuracy;
+}
+
+function displayStats() {
+  let storeResult = calculateAccuracy().toFixed(0);
+  console.log(storeResult)
+  $('.attempts-value-text').text(attempts);
+  $('.accuracy-value-text').text(storeResult + '%');
+  $('.games-played-value-text').text(games_played);
 }
