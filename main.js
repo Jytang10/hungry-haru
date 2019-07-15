c76876aa8813146ede06876cff83381709d61cf6
James Tang <jytang10@gmail.com>
2019-07-15 04:25:02 +0000

    Added sound config icons to aside

--- main.js
+++ main.js
@@ -186,3 +186,29 @@ function move() {
     default:
   }
 }
+
+// function sound(src) {
+//   this.sound = document.createElement("audio");
+//   this.sound.src = src;
+//   this.sound.setAttribute("preload", "auto");
+//   this.sound.setAttribute("controls", "none");
+//   this.sound.style.display = "none";
+//   document.body.appendChild(this.sound);
+//   this.play = function(){
+//     this.sound.play();
+//   }
+//   this.stop = function(){
+//     this.sound.pause();
+//   }
+// }
+
+// var mySound;
+// var myMusic;
+
+// function startGame() {
+//   // myGamePiece = new component(30, 30, "red", 10, 120);
+//   // mySound = new sound("bounce.mp3");
+//   myMusic = new sound("theme.mp3");
+//   myMusic.play();
+//   myGameArea.start();
+// }
